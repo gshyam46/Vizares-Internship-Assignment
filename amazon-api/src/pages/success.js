3d93d68da1cde8f8780f1d231be601935c50a78d
Ghanashyam G <gshyam2603@gmail.com>
2024-11-18 10:58:02 +0530

    Completed api-integration for amazon frontend

---
+++ amazon-api/src/pages/success.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Header from "../components/Header";
+import { CheckCircleIcon } from "@heroicons/react/24/solid";
+import { useRouter } from "next/router";
+
+function success() {
+  const router = useRouter();
+
+  return (
+    <div className="bg-gray-100 h-screen">
+      <Header />
+      <main className="max-w-screen-lg mx-auto">
+        <div className="flex flex-col p-10 bg-white">
+          <div className="flex items-center space-x-2 mb-5">
+            <CheckCircleIcon className="text-green-500 h-10" />
+            <h1>Thank you, your order has been confirmed!</h1>
+          </div>
+          <p>
+            Thank you for shopping with us. We'll send a confirmation once the
+            item has been shipped.
+          </p>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default success;
